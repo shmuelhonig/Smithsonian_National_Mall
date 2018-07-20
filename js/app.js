34823faeaa13e3c3b20d03b90696d838bc175bc4
Shmuel Honig <shonig@gmail.com>
2018-07-20 15:20:41 -0400

    Add Smithsonian locations

---
+++ js/app.js
@@ -0,0 +1,121 @@
+var locations = [
+  {
+    title: "Anacostia Community Museum",
+    coord: {
+      lat: 38.856562,
+      lng: -76.976677
+    }
+  },
+  {
+    title: "Arthur M. Sackler Gallery",
+    coord: {
+      lat: 38.887941,
+      lng: -77.026354
+    }
+  },
+  {
+    title: "Arts and Industries Building",
+    coord: {
+      lat: 38.88818,
+      lng: -77.0246
+    }
+  },
+  {
+    title: "Freer Gallery of Art",
+    coord: {
+      lat: 38.888075,
+      lng: -77.0274
+    }
+  },
+  {
+    title: "Hirshhorn Museum and Sculpture Garden",
+    coord: {
+      lat: 38.888161,
+      lng: -77.022968
+    }
+  },
+  {
+    title: "National Air and Space Museum",
+    coord: {
+      lat: 38.88816,
+      lng: -77.019868
+    }
+  },
+  {
+    title: "National Museum of African American History and Culture",
+    coord: {
+      lat: 38.891064,
+      lng: -77.032614
+    }
+  },
+  {
+    title: "National Museum of African Art",
+    coord: {
+      lat: 38.887933,
+      lng: -77.025507
+    }
+  },
+  {
+    title: "National Museum of American History",
+    coord: {
+      lat: 38.891279,
+      lng: -77.030051
+    }
+  },
+  {
+    title: "National Museum of the American Indian",
+    coord: {
+      lat: 38.888348,
+      lng: -77.016503
+    }
+  },
+  {
+    title: "National Museum of Natural History",
+    coord: {
+      lat: 38.891266,
+      lng: -77.026065
+    }
+  },
+  {
+    title: "National Portrait Gallery",
+    coord: {
+      lat: 38.897869,
+      lng: -77.023053
+    }
+  },
+  {
+    title: "National Postal Museum",
+    coord: {
+      lat: 38.898097,
+      lng: -77.008238
+    }
+  },
+  {
+    title: "Renwick Gallery",
+    coord: {
+      lat: 38.89914,
+      lng: -77.039032
+    }
+  },\
+  {
+    title: "Smithsonian American Art Museum",
+    coord: {
+      lat: 38.897861,
+      lng: -77.022942
+    }
+  },
+  {
+    title: "Smithsonian Institution Building (The Castle)",
+    coord: {
+      lat: 38.888786,
+      lng: -77.026023
+    }
+  },
+  {
+    title: "National Zoological Park (National Zoo)",
+    coord: {
+      lat: 38.929616,
+      lng: -77.049784
+    }
+  }
+]
